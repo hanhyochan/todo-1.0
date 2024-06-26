7e319d93ebc1fc750079a832d4c79b7f68b73662
hanhyochan <hcartist569@gmail.com>
2024-06-27 01:02:00 +0900

    06/27

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
         <input
           value={id}
           onChange={handleId}
-          placeholder="아이디를 입력해주세요"
+          placeholder="아이디를 입력해주세요hh"
         />{" "}
         <br />
         <input
