47344c2b80badaf3f1ce3575d0de31cf3151796a
hanhyochan <hcartist569@gmail.com>
2024-08-16 13:23:15 +0900

    08/16

---
+++ src/Page/Todo/Main.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+
+export default function Main() {
+  const savedTodoList = JSON.parse(localStorage.getItem('todoList'));
+
+  const [value, setValue] = useState(new Date());
+  const [content, setContent] = useState("");
+  const [todoList, setTodoList] = useState(savedTodoList);
+  const [view, setView] = useState(true);
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+  }, [todoList]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (content.trim() === "") return;
+
+    const todoInfo = {
+      id: `todo-${Date.now()}`,
+      content: content,
+      checked: false,
+    };
+    setTodoList([...todoList, todoInfo]);
+    setContent("");
+  }
+
+  const handleCheckbox = (id) => {
+    setTodoList(todoList.map(x => 
+      x.id === id ? { ...x, checked: !x.checked } : x
+    ));
+  };
+
+  const deleteBtn = (id) => {
+    const updatedTodoList = todoList.filter(todoList => todoList.id !== id);
+    setTodoList(updatedTodoList)
+  };
+
+
+  const renderedTodoList = todoList.map((x) => (
+    <div key={x.id}>
+      <label>
+        <input
+          type="checkbox"
+          checked={x.checked}
+          onChange={() => handleCheckbox(x.id)}
+        />
+        <span
+        onClick={()=>console.log(x.id)}
+        >{x.content}</span>
+        <button
+        onClick={() => deleteBtn(x.id)}
+        >삭제wp</button>
+        <button
+        onClick={() => setView(false)}
+        >수정</button>
+      </label>
+
+    </div>
+  ));
+
+  const editTodoList = (
+    <div>하이</div>
+  )
+
+
+
+  return (
+    <>
+      <Calendar onClickDay={setValue} />
+      <div>{moment(value).format("YYYY년 MM월 DD일")}</div>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="할일을 입력해주세요.."
+          />
+          <button>입력</button>
+        </form>
+      </div>
+
+      <div>{view ? renderedTodoList : editTodoList}</div>
+    </>
+  );
+}
